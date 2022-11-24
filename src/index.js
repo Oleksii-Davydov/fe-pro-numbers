707f986832701159749211bb950f36feba40932e
Oleksii-Davydov <davydovalekcey1992@gmail.com>
2022-11-24 19:28:48 +0200

    f2

--- src/index.js
+++ src/index.js
@@ -7,13 +7,11 @@
  * @param {number} maxSecond
  * @returns {number}
  */
-let minFirst =2;
-let minSecond =3;
-let maxFirst =4;
-let maxSecond =5;
+
 
 export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {
-    console.log(Math.min(minFirst,minSecond) * Math.max(maxFirst,maxSecond))}
+    return Math.min(minFirst, minSecond) * Math.max(maxFirst, maxSecond)
+}
 /**
  * Функция принимает один числовой аргумент, это радиус круга. Функция должна
  * вернуть площадь круга, формулу найдите в интернете, если не помните
@@ -21,8 +19,10 @@ export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {
  * @param {number} radius
  * @returns {number}
  */
-let radius = 4;
-export const getSquareOfCircle = (radius) => {console.log(Math.PI*Math.pow(radius, 2))};
+
+export const getSquareOfCircle = (radius) => {
+    return Math.PI * Math.pow(radius, 2)
+};
 
 /**
  * Функция принимает число и точность к которой нужно округлить
@@ -32,32 +32,23 @@ export const getSquareOfCircle = (radius) => {console.log(Math.PI*Math.pow(radiu
  * @param {number} accuracy
  * @returns {number}
  */
-let numberN = 3.2345
-let accuracyN = 3
-export const getFixedValue = (numberN, accuracyN) => {
-    console.log(Math.round(parseFloat(numberN) * 1000) / 1000)};
-//export const getFixedValue = (numberN, accuracyN) => {if (!Math.round10) {
-  //  Math.round10 = function(numberN, accuracyN) {
-      //  console.log(Math.round10 (numberN,accuracyN))}
-   // };
 
-//}
+export const getFixedValue = (number, accuracy) => {
+    return Math.round(number * Math.pow(10, accuracy)) / Math.pow(10, accuracy);
+};
 
-//if (!Math.round10) { Math.round10 = function(value, exp) { return decimalAdjust('round', value, exp); }
 /**
  * Если целая часть числа четная, то срабатывает Math.floor для этого числа
  * если нечетное, то Math.ceil
  * @param {number} number
  * @returns {number}
  */
-let numberX = 6.235
-export const getFlooroCeil = (numberX) => {
-    if (Math.trunc(numberX)%2 === 0) {
-        console.log(Math.floor(numberX) )
-    }
-    else {
-        console.log(Math.ceil(numberX))
 
+export const getFlooroCeil = (number) => {
+    if (Math.trunc(number) % 2 === 0) {
+        return Math.floor(number);
+    } else {
+        return Math.ceil(number);
     }
 };
 
@@ -69,7 +60,7 @@ export const getFlooroCeil = (numberX) => {
  * @param {number} to
  * @returns {number}
  */
-let fromNumber =2
-let toNumber =5
-export const randomizer = (fromNumber, toNumber) => {
-    console.log(Math.floor(Math.random()*(toNumber-fromNumber)+fromNumber))};
+
+export const randomizer = (from, to) => {
+    return Math.round(Math.random() * (to - from) + from)
+};
